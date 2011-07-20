379d0637f3b4ef582771a64c143a42e99ad7df6e
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2011-07-21 01:12:07 +0200

    netbeans

--- src/de/xghostkillerx/unlimitedlava/UnlimitedLava.java
+++ src/de/xghostkillerx/unlimitedlava/UnlimitedLava.java
@@ -8,27 +8,25 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
-* UnlimitedLava for CraftBukkit
-*
-* @author xGhOsTkiLLeRx
-*/
+ * UnlimitedLava for CraftBukkit
+ *
+ * @author xGhOsTkiLLeRx
+ */
+public class UnlimitedLava extends JavaPlugin {
 
-public class UnlimitedLava extends JavaPlugin
-{
-public static final Logger log = Logger.getLogger("Minecraft");
-private final UnlimitedLavaBlockListener blockListener = new UnlimitedLavaBlockListener(this);
+    public static final Logger log = Logger.getLogger("Minecraft");
+    private final UnlimitedLavaBlockListener blockListener = new UnlimitedLavaBlockListener(this);
 
 //Shutdown
-public void onDisable() {
-PluginDescriptionFile pdfFile = this.getDescription();
-        log.info(pdfFile.getName()+" " +pdfFile.getVersion()+" has been disabled!");
-}
+    public void onDisable() {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " has been disabled!");
+    }
 //Start
-public void onEnable() {
-PluginDescriptionFile pdfFile = this.getDescription();
-PluginManager pm = getServer().getPluginManager();
-log.info(pdfFile.getName()+" " +pdfFile.getVersion()+" is enabled!");
-pm.registerEvent(Event.Type.BLOCK_FROMTO,blockListener,Event.Priority.Normal,this);
-}
-
+    public void onEnable() {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        PluginManager pm = getServer().getPluginManager();
+        log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " is enabled!");
+        pm.registerEvent(Event.Type.BLOCK_FROMTO, blockListener, Event.Priority.Normal, this);
+    }
 }
--- src/de/xghostkillerx/unlimitedlava/UnlimitedLavaBlockListener.java
+++ src/de/xghostkillerx/unlimitedlava/UnlimitedLavaBlockListener.java
@@ -5,61 +5,61 @@ import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockListener;
 
 /**
-* UnlimitedLava block listener
-* @author xGhOsTkiLLeRx
-*/
+ * UnlimitedLava block listener
+ * @author xGhOsTkiLLeRx
+ */
+public class UnlimitedLavaBlockListener extends BlockListener {
 
+    public static UnlimitedLava plugin;
 
-public class UnlimitedLavaBlockListener extends BlockListener
-{
-  public static UnlimitedLava plugin;
+    public UnlimitedLavaBlockListener(UnlimitedLava instance) {
+        plugin = instance;
+    }
 
-  public UnlimitedLavaBlockListener(UnlimitedLava instance)
-  {
-    plugin = instance;
-  }
+    public void onBlockFromTo(BlockFromToEvent event) {
+        if (!event.isCancelled()) {
+            if (event.getBlock().getTypeId() == 11) {
+                if (surounded(event.getToBlock())) {
+                    event.getToBlock().setTypeId(11);
+                }
+            }
+        }
+    }
 
-  public void onBlockFromTo(BlockFromToEvent event)
-  {
-if (!event.isCancelled())
-{
-    if (event.getBlock().getTypeId() == 11)
-     {
-      if (surounded(event.getToBlock()))
-       {
-        event.getToBlock().setTypeId(11);
-       }
-     }
-}
-  }
+    private boolean surounded(Block block) {
+        if (block.getTypeId() != 10) {
+            return false;
+        }
+        int n = 0;
 
-  private boolean surounded(Block block)
-  {
-    if (block.getTypeId() != 10)
-      return false;
-    int n = 0;
+        if (spawnable(block, 1, 0)) {
+            n++;
+        }
+        if (spawnable(block, -1, 0)) {
+            n++;
+        }
+        if (spawnable(block, 0, 1)) {
+            n++;
+        }
+        if (spawnable(block, 0, -1)) {
+            n++;
+        }
+        if (spawnable(block, 1, -1)) {
+            n++;
+        }
+        if (spawnable(block, 1, 1)) {
+            n++;
+        }
+        if (spawnable(block, -1, -1)) {
+            n++;
+        }
+        if (spawnable(block, -1, 1)) {
+            n++;
+        }
+        return n == 8;
+    }
 
-    if (spawnable(block, 1, 0))
-      n++;
-    if (spawnable(block, -1, 0))
-      n++;
-    if (spawnable(block, 0, 1))
-      n++;
-    if (spawnable(block, 0, -1))
-      n++;
-    if (spawnable(block, 1, -1))
-      n++;
-    if (spawnable(block, 1, 1))
-      n++;
-    if (spawnable(block, -1, -1))
-      n++;
-    if (spawnable(block, -1, 1))
-      n++;
-    return n == 8;
-  }
-
-  private boolean spawnable(Block block, int x, int z)
-  {
-    return ((block.getRelative(x, 0, z).getTypeId() == 10) || (block.getRelative(x, 0, z).getTypeId() == 11)) && (block.getRelative(x, 0, z).getData() == 0);
-  }
+    private boolean spawnable(Block block, int x, int z) {
+        return ((block.getRelative(x, 0, z).getTypeId() == 10) || (block.getRelative(x, 0, z).getTypeId() == 11)) && (block.getRelative(x, 0, z).getData() == 0);
+    }
 }
