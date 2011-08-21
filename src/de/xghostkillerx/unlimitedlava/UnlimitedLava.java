100e0cf30cb464821512b43dab7eff6ed9bd9491
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2011-08-21 19:41:58 +0200

    final changes for 1.2!

--- src/de/xghostkillerx/unlimitedlava/UnlimitedLava.java
+++ src/de/xghostkillerx/unlimitedlava/UnlimitedLava.java
@@ -23,18 +23,18 @@ import org.bukkit.util.config.Configuration;
  */
 
 public class UnlimitedLava extends JavaPlugin {
-	
+
 	public Configuration config;
-    public Boolean three;
-    public Boolean two;
-    public Boolean other;
-    public Boolean big;
-    public Boolean permissions;
-    public static final Logger log = Logger.getLogger("Minecraft");
+	public Boolean three;
+	public Boolean two;
+	public Boolean other;
+	public Boolean big;
+	public Boolean permissions;
+	public static final Logger log = Logger.getLogger("Minecraft");
 	private final UnlimitedLavaBlockListener blockListener = new UnlimitedLavaBlockListener(
 			this);
 
-    // Shutdown
+	// Shutdown
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " has been disabled!");
@@ -45,31 +45,32 @@ public class UnlimitedLava extends JavaPlugin {
 		// Events
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.BLOCK_FROMTO, blockListener, Event.Priority.Normal, this);
-        
-        // Config
-        config = getConfiguration();
-        reloadConfig();
-        config.save();
-        
-        // Message
-        PluginDescriptionFile pdfFile = this.getDescription();
-        log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " is enabled!");
-    }
 
-	// Reload the config file, via command /unlimitedlava reload and at the start!
-    public void reloadConfig() {
-    	config.load();
-        config.setHeader("# If you haven't understood the config (especially the point 'other'),", "# please refer to this topic: http://bit.ly/n1Wex2");
-        three = config.getBoolean("three", true);
-        two = config.getBoolean("two", true);
-        other = config.getBoolean("other", false);
-        big = config.getBoolean("big", false);
-        permissions = config.getBoolean("permissions", true);
-        config.save();
-    }
-    
-    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-    	UnlimitedLavaCommands cmd = new UnlimitedLavaCommands(this);
-    	return cmd.UnlimitedLavaCommand(sender, command, commandLabel, args);
-    	}
+		// Config
+		config = getConfiguration();
+		reloadConfig();
+		config.save();
+
+		// Message
+		PluginDescriptionFile pdfFile = this.getDescription();
+		log.info(pdfFile.getName() + " " + pdfFile.getVersion()	+ " is enabled!");
+	}
+
+	// Reload the config file, via command /unlimitedlava reload or /ulava reload and at the start!
+	public void reloadConfig() {
+		config.load();
+		config.setHeader("# If you haven't understood the config (especially the point 'other' and 'big'),", "# please refer to this topic: http://bit.ly/n1Wex2");
+		three = config.getBoolean("three", true);
+		two = config.getBoolean("two", true);
+		other = config.getBoolean("other", false);
+		big = config.getBoolean("big", false);
+		permissions = config.getBoolean("permissions", true);
+		config.save();
+	}
+
+	public boolean onCommand(CommandSender sender, Command command,
+			String commandLabel, String[] args) {
+		UnlimitedLavaCommands cmd = new UnlimitedLavaCommands(this);
+		return cmd.UnlimitedLavaCommand(sender, command, commandLabel, args);
+	}
 }
