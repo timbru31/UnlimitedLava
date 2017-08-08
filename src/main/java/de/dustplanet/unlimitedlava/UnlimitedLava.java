f10c4e28b6dfb20cfb823899547894b70bced1e1
Tim Brust <ratchet.player@gmx.de>
2017-08-08 18:51:41 +0200

    chore: build against Bukkit 1.12.1

--- src/main/java/de/dustplanet/unlimitedlava/UnlimitedLava.java
+++ src/main/java/de/dustplanet/unlimitedlava/UnlimitedLava.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.bstats.Metrics;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -27,8 +27,7 @@ public class UnlimitedLava extends JavaPlugin {
     private File configFile, localizationFile;
     @Getter
     @Setter
-    private boolean three, two, other, big, plus, tShape, ring, lavaFall, waterFall, messages = true,
-            permissions = true, furnace, debug;
+    private boolean three, two, other, big, plus, tShape, ring, lavaFall, waterFall, messages = true, permissions = true, furnace, debug;
     private FileConfiguration config;
     @Getter
     private FileConfiguration localization;
@@ -121,8 +120,7 @@ public class UnlimitedLava extends JavaPlugin {
         config.addDefault("sources.lava_fall", true);
         config.addDefault("sources.water_fall", false);
         config.addDefault("furnace.item", "BUCKET");
-        List<String> worlds = getServer().getWorlds().stream().map(w -> w.getUID().toString())
-                .collect(Collectors.toList());
+        List<String> worlds = getServer().getWorlds().stream().map(w -> w.getUID().toString()).collect(Collectors.toList());
         config.addDefault("enabled_worlds", worlds);
         config.addDefault("debug", false);
         config.options().copyDefaults(true);
@@ -143,32 +141,31 @@ public class UnlimitedLava extends JavaPlugin {
         messages = config.getBoolean("configuration.messages");
         furnace = config.getBoolean("configuration.furnace");
         height = config.getInt("configuration.height");
-        enabledWorlds = config.getStringList("enabled_worlds").stream().map(g -> UUID.fromString(g))
-                .collect(Collectors.toList());
+        enabledWorlds = config.getStringList("enabled_worlds").stream().map(g -> UUID.fromString(g)).collect(Collectors.toList());
         debug = config.getBoolean("debug");
     }
 
     public void loadLocalization() {
         localization.addDefault("permission_denied", "&4You do not have the permission to do this!");
         localization.addDefault("reload", "&2UnlimitedLava &4%version &2reloaded!");
-        localization.addDefault("help", "&2Welcome to the UnlimitedLava version &4%version &2help\n"
-                + "To see the help type &4/unlimitedlava help &for &4/ulava help\n"
-                + "To reload use &4/unlimitedlava reload &for &4/ulava reload\n"
-                + "To enable something use &4/unlimitedlava enable &e<value>\n" + "or &4/ulava enable &e<value>\n"
-                + "To disable something use &4/unlimitedlava disable &e<value>\n" + "or &4/ulava disable &e<value>\n"
-                + "&eValues: &fpermissions, messages, furnace, all, three, two, other, big, lava_fall, water_fall, plus, T\n"
-                + "&eThe status of UnlimitedLava can be seen with &4/ulava status");
+        localization.addDefault("help",
+                "&2Welcome to the UnlimitedLava version &4%version &2help\n"
+                        + "To see the help type &4/unlimitedlava help &for &4/ulava help\n"
+                        + "To reload use &4/unlimitedlava reload &for &4/ulava reload\n"
+                        + "To enable something use &4/unlimitedlava enable &e<value>\n" + "or &4/ulava enable &e<value>\n"
+                        + "To disable something use &4/unlimitedlava disable &e<value>\n" + "or &4/ulava disable &e<value>\n"
+                        + "&eValues: &fpermissions, messages, furnace, all, three, two, other, big, lava_fall, water_fall, plus, T\n"
+                        + "&eThe status of UnlimitedLava can be seen with &4/ulava status");
         localization.addDefault("enable_source", "&2UnlimitedLava source &4%source &2enabled!");
         localization.addDefault("enable_all", "&4All &2UnlimitedLava sources enabled!");
         localization.addDefault("enable_messages", "&2UnlimitedLava messages enabled!");
-        localization.addDefault("enable_permissions", "&2UnlimitedLava permissions enabled!\n"
-                + "&2Only OPs or players with the permission can use the plugin!");
+        localization.addDefault("enable_permissions",
+                "&2UnlimitedLava permissions enabled!\n" + "&2Only OPs or players with the permission can use the plugin!");
         localization.addDefault("enable_furnace", "&2UnlimitedLava &4furnace &2enabled!");
         localization.addDefault("disable_source", "&2UnlimitedLava source &4%source &2disabled!");
         localization.addDefault("disable_all", "&4All &2UnlimitedLava sources disabled!");
         localization.addDefault("disable_messages", "&2UnlimitedLava messages disabled!");
-        localization.addDefault("disable_permissions",
-                "&2UnlimitedLava permissions disabled!\n" + "&4All players can use the plugin!");
+        localization.addDefault("disable_permissions", "&2UnlimitedLava permissions disabled!\n" + "&4All players can use the plugin!");
         localization.addDefault("disable_furnace", "&2UnlimitedLava &4furnace &2disabled!");
         localization.options().copyDefaults(true);
         saveLocalization();
@@ -218,8 +215,9 @@ public class UnlimitedLava extends JavaPlugin {
             newValue = "";
         }
         PluginDescriptionFile pdfFile = this.getDescription();
-        String[] msg = ChatColor.translateAlternateColorCodes('\u0026', message
-                .replace("%version", pdfFile.getVersion()).replace("%source", newValue).replace("%value", newValue))
+        String[] msg = ChatColor
+                .translateAlternateColorCodes('\u0026',
+                        message.replace("%version", pdfFile.getVersion()).replace("%source", newValue).replace("%value", newValue))
                 .split("\n");
         if (player != null) {
             player.sendMessage(msg);
