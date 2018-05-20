6ec4d47c8d15c20935fafc53f8694bcb1dded291
Tim Brust <github@timbrust.de>
2018-05-20 15:51:15 +0200

    feat: prepare for Minecraft 1.13

--- src/main/java/de/dustplanet/unlimitedlava/UnlimitedLavaBlockListener.java
+++ src/main/java/de/dustplanet/unlimitedlava/UnlimitedLavaBlockListener.java
@@ -3,6 +3,7 @@ package de.dustplanet.unlimitedlava;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Levelled;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
@@ -31,38 +32,38 @@ public class UnlimitedLavaBlockListener implements Listener {
         }
 
         if (plugin.isLavaFall() && sourceBlock.getY() > 0 && event.getFace() == BlockFace.DOWN) {
-            if (event.getToBlock().getRelative(BlockFace.UP).getType() == Material.LAVA
-                    || event.getToBlock().getRelative(BlockFace.UP).getType() == Material.STATIONARY_LAVA) {
-                if (event.getToBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
-                    event.getToBlock().setTypeIdAndData(Material.LAVA.getId(), (byte) 0x0, true);
+            if (targetBlock.getRelative(BlockFace.UP).getType() == Material.LAVA) {
+                if (targetBlock.getRelative(BlockFace.DOWN).getType() != Material.AIR) {
+                    Levelled targetLevel = (Levelled) targetBlock.getBlockData();
+                    targetLevel.setLevel(0);
                     event.setCancelled(true);
                 }
             }
         }
 
         if (plugin.isWaterFall() && sourceBlock.getY() > 0 && event.getFace() == BlockFace.DOWN) {
-            if (event.getToBlock().getRelative(BlockFace.UP).getType() == Material.WATER
-                    || event.getToBlock().getRelative(BlockFace.UP).getType() == Material.STATIONARY_WATER) {
-                if (event.getToBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
-                    event.getToBlock().setTypeIdAndData(Material.WATER.getId(), (byte) 0x0, true);
+            if (targetBlock.getRelative(BlockFace.UP).getType() == Material.WATER) {
+                if (targetBlock.getRelative(BlockFace.DOWN).getType() != Material.AIR) {
+                    Levelled targetLevel = (Levelled) targetBlock.getBlockData();
+                    targetLevel.setLevel(0);
                     event.setCancelled(true);
                 }
             }
         }
 
-        if (sourceBlock.getData() != 0x0) {
+        if (!(sourceBlock.getBlockData() instanceof Levelled) || ((Levelled) sourceBlock.getBlockData()).getLevel() != 0) {
             return;
         }
 
-        if (sourceBlock.getType() == Material.LAVA || sourceBlock.getType() == Material.STATIONARY_LAVA) {
-            if (targetBlock.getType() == Material.LAVA || targetBlock.getType() == Material.STATIONARY_LAVA) {
-                // Full block (0x0) and not falling (0x8)
-                if (targetBlock.getData() != 0x0 && targetBlock.getData() != 0x8) {
-                    if (check.checkLavaSpreadValidity(targetBlock)) {
-                        targetBlock.setTypeIdAndData(Material.LAVA.getId(), (byte) 0x0, true);
-                    }
+        if (sourceBlock.getType() == Material.LAVA && targetBlock.getType() == Material.LAVA) {
+            Levelled targetLevel = (Levelled) targetBlock.getBlockData();
+            // Full block (0x0) and not falling (0x8)
+            if (targetLevel.getLevel() != 0 && targetLevel.getLevel() != 8) {
+                if (check.checkLavaSpreadValidity(targetBlock)) {
+                    targetLevel.setLevel(0);
                 }
             }
         }
+
     }
 }
